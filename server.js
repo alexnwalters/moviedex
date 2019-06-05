1e425fc3c0bf660ab845d7d361c4f3f4f9332771
Alex Walters <alexnwalters@gmail.com>
2019-06-04 21:06:51 -0400

    correct PORT error

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use((error, req, res, next) => {
     res.status(500).json(response)
 })
 
-const PORT = precess.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
